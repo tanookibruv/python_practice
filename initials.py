0dfc4e7dd26e8ac6480246a700311a84b46dba1c
tanookibruv <erendon1836@gmail.com>
2023-02-09 18:03:36 -0800

    wrapped up part 1 in python via codedex

---
+++ initials.py
@@ -0,0 +1,9 @@
+#My name is Esteban Rendon, I really like video games and computers 🤓
+
+print('EEEEEEEE   RRRRRRR')
+print('EE         RR    RRR')
+print('EE         RR    RR')
+print('EEEEEE     RRRRRRR')
+print('EE         RR     RR')
+print('EE         RR      RR')
+print('EEEEEEEE   RR      RR')
---
+++ letter.py
@@ -0,0 +1,5 @@
+#...to my future self...
+print('In 3 months, I will have made better progress in my weight loss journey')
+print('In 3 months, I will have a better understanding of JS and Python')
+print('In 3 months, I will have closed my biggest debt commitments')
+print('In 3 months, I will have been promoted')
